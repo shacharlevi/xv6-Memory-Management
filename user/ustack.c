01e9dd24dd46f9196de142fd05db24f02f5d538c
shacharlevi <lshachar123@gmail.com>
2023-05-21 11:35:00 +0300

    1

--- user/ustack.c
+++ user/ustack.c
@@ -2,45 +2,65 @@
 #include "user/user.h"
 #include "riscv.h"
 
-static Header *top = 0;
+static void *top = 0;
+static void* base = 0;
 static int counter = -1;
-
+static uint prevLen = 0;
 void *ustack_malloc(uint len)
 {
     if (len > 512)
     {
         return (void *)-1;
     }
-    if (counter >= PGSIZE)
+    /*if (counter >= PGSIZE)
+    {
+        return (void *)-1;
+    }*/
+
+    // uint nUnits = (len + sizeof(Header) - 1) / sizeof(Header) + 1;
+    // Header *pointer = (Header *)sbrk(nUnits * sizeof(Header));
+    void *pointerLen = (void *)sbrk(8);
+    if (pointerLen == (void *)-1)
     {
         return (void *)-1;
     }
-
-    uint nUnits = (len + sizeof(Header) - 1) / sizeof(Header) + 1;
-    Header *pointer = (Header *)sbrk(nUnits * sizeof(Header));
-    // Header *pointer = (Header *)sbrk(len);
-
-    if (pointer == (Header *)-1)
+    *((uint*)pointerLen + 1) = len;
+    *((uint*) pointerLen) = prevLen;
+    void *pointer = (void *)sbrk(len);
+    prevLen = len;
+    if (pointer == (void *)-1)
     {
         return (void *)-1;
     }
-    pointer->s.size = nUnits;
-    if (top == 0)
-    {
-        pointer->s.ptr = 0;
+    if(base == 0){
+        base = pointer;
     }
-    else
-    {
-        pointer->s.ptr = top;
-    }
-    counter++;
     top = pointer;
-    return (void *)(pointer + 1);
+    return pointer;
 }
 
 int ustack_free(void)
 {
-    Header *p;
+    if(base == 0){
+        return -1;
+    }
+    int isLast = 0;
+    if(base == top){
+        isLast = 1;
+    }
+    uint len = *((uint*)top-1);
+    uint prevLen = *((uint*)top-2);
+    void* newTop = (void*)sbrk(-len - 8);
+    if(newTop == (void*) -1){
+        return -1;
+    }
+    if(isLast == 1){
+        top = 0;
+        base = 0;
+    } else {
+        top = (void*)((char*)newTop - prevLen);
+    }
+    /*Header *p;
     int ret;
     if (counter == -1)
     {
@@ -62,5 +82,5 @@ int ustack_free(void)
         top = p->s.ptr;
     }
     counter--;
-    return ret;
+    return ret;*/
 }
